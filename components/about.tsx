1bb46bcc4193c1875fa7fd6ae1021bc759ed7152
florescente <florescente@gmail.com>
2022-06-28 09:45:43 -0300

    menu done

---
+++ components/about.tsx
@@ -0,0 +1,7 @@
+import MenuIcon from '@mui/icons-material/Menu'
+
+function About() {
+  return <MenuIcon />
+}
+
+export default About
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material'
 import { Router } from 'next/router'
 import React from 'react'
+import About from './about'
 import Info from './Info'
 import MenuComponent from './menu'
 
@@ -37,10 +38,15 @@ function Layout({
           height="3rem"
           marginBottom={3}
           paddingRight={{ xs: '32px', lg: '32px' }}
+          paddingLeft="32px"
           paddingY="1rem"
           display="flex"
-          justifyContent="flex-end"
+          justifyContent={{ xs: 'space-between', lg: 'flex-end' }}
+          flexDirection={{ xs: 'row-reverse', lg: 'row' }}
         >
+          <Box alignItems="center" display={{ xs: 'flex', lg: 'none' }}>
+            <About />
+          </Box>
           <MenuComponent path={router.asPath} />
         </Box>
         <Box
@@ -48,7 +54,7 @@ function Layout({
           flexGrow={1}
           marginRight={{ xs: '32px', lg: '32px' }}
           marginLeft={{ xs: '32px', lg: '0px' }}
-          paddingLeft="20px"
+          paddingLeft={{ xs: '0px', lg: '20px' }}
           justifyItems="center"
         >
           {children}
