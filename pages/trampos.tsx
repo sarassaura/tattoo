022da2faadc8155cfab2fb5118cd10d8dacbb5c7
florescente <florescente@gmail.com>
2022-07-07 20:46:59 -0300

    trampos teste

--- pages/trampos.tsx
+++ pages/trampos.tsx
@@ -7,6 +7,9 @@ function trampos({
   nextCursor: defaultNextCursor,
   folders,
 }: TramProps) {
+  console.log('trampos images', defaultImages)
+  console.log('trampos cursor', defaultNextCursor)
+  console.log('trampos folders', folders)
   return (
     <Gallery
       defaultimages={defaultImages}
