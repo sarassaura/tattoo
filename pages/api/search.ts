7ce7e3d43277b10fa6b526974b11daad540bc505
florescente <florescente@gmail.com>
2022-07-08 12:32:57 -0300

    dotenv why

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import {} from 'dotenv/config'
+import 'dotenv/config'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { search } from '../../utils/cloudinary'
 
