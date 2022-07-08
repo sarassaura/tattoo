b8532d0b92160e35ba01f0eec4ba225245625982
florescente <florescente@gmail.com>
2022-07-08 12:49:09 -0300

    like a brick

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,11 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import dotenv from 'dotenv'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { search } from '../../utils/cloudinary'
 
-dotenv.config()
+require('dotenv').config()
 
-console.log('unknown api key hahaha', process.env.CLOUDINARY_API_KEY)
+export async function search(options: any = {}) {
+  const params = {
+    ...options,
+  }
+  if (options.nextCursor) {
+    params.next_cursor = options.nextCursor
+    delete params.nextCursor
+  }
+  const paramString = Object.keys(params)
+    .map((key) => `${key}=${encodeURIComponent(params[key])}`)
+    .join('&')
+  const results = await fetch(
+    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_NAME}/resources/search?${paramString}`,
+    {
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}`
+        ).toString('base64')}`,
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+    }
+  ).then((r) => r.json())
+  return results
+}
 
 export default async function handler(
   req: NextApiRequest,
