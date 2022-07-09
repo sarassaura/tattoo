5414ce64bd17b0bbcc7c8ba8d3e5ecfb737d8417
florescente <florescente@gmail.com>
2022-07-09 18:15:18 -0300

    some headers maybe

--- next.config.js
+++ next.config.js
@@ -5,6 +5,35 @@ const nextConfig = {
     domains: ['res.cloudinary.com'],
     contentSecurityPolicy: ``,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
