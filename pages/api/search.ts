aaf32e0b71aeced8fd00bde4a7a1e82595c03f5e
florescente <florescente@gmail.com>
2022-07-09 17:53:03 -0300

    search direct

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { search } from '../../utils/cloudinary'
 
 require('dotenv').config()
 
@@ -7,28 +8,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const parameters = JSON.parse(req.body)
-  // start
-  const params = { ...parameters }
-  if (parameters.nextCursor) {
-    params.next_cursor = parameters.nextCursor
-    delete params.nextCursor
-  }
-  const paramString = Object.keys(params)
-    .map((key) => `${key}=${encodeURIComponent(params[key])}`)
-    .join('&')
-  const results = await fetch(
-    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_NAME}/resources/search?${paramString}`,
-    {
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          `${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}`
-        ).toString('base64')}`,
-      },
-    }
-  )
-  const definitive = await results.json()
-  // end
+  const params = JSON.parse(req.body)
+  const definitive = await search(params)
   res.status(200).json({
     ...definitive,
   })
