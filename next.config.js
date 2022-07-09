91195fffbfdade875851dba77188ae5d225219e3
florescente <florescente@gmail.com>
2022-07-09 14:17:08 -0300

    x-content-type-options

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+]
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com'],
