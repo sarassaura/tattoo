09e70c24a9d00c4fc5fd6e8af4d2ecee2bf3ca44
florescente <florescente@gmail.com>
2022-07-15 23:28:39 -0300

    im trying

--- next.config.js
+++ next.config.js
@@ -8,7 +8,11 @@ const nextConfig = {
     contentSecurityPolicy: ``,
     /*     loader: 'cloudinary',
     path: 'https://res.cloudinary.com', */
-    // https:res.cloudinary.com/campoflorescente/f_auto,c_limit,w_1920,q_auto/https://res.cloudinary.com/campoflorescente/image/upload/v1656849844/lagarto-snake.webp
+    // https://res.cloudinary.com/campoflorescente/f_auto,c_limit,w_1920,q_auto/https://res.cloudinary.com/campoflorescente/image/upload/v1656849844/lagarto-snake.webp
+    ///                                            _next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fcampoflorescente%2Fimage%2Fupload%2Fv1656849844%2Flagarto-original.webp&w=828&q=75
+    // https://vitor-underground-tattoo.vercel.app/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fcampoflorescente%2Fimage%2Fupload%2Fv1656849844%2Flagarto-snake.webp&w=828&q=75
+    loader: 'cloudinary',
+    path: 'https://vitor-underground-tattoo.vercel.app/image/upload/',
   },
   i18n,
   async headers() {
