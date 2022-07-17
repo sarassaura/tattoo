3e7ec46dcb9e0a651ce43b13654a8cdf43685a36
florescente <florescente@gmail.com>
2022-07-16 23:04:04 -0300

    dont work at all

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,18 +1,6 @@
-const HttpBackend = require('i18next-http-backend/cjs')
-const ChainedBackend = require('i18next-chained-backend').default
-const LocalStorageBackend = require('i18next-localstorage-backend').default
-
-const isBrowser = typeof window !== 'undefined'
-
 module.exports = {
-  backend: {
-    backendOptions: [{ expirationTime: 60 * 60 * 1000 }, {}], // 1 hour
-    backends: isBrowser ? [LocalStorageBackend, HttpBackend] : [],
-  },
   i18n: {
     locales: ['pt-BR', 'en'],
     defaultLocale: 'pt-BR',
   },
-  serializeConfig: false,
-  use: isBrowser ? [ChainedBackend] : [],
 }
