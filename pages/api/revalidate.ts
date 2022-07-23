6d25b519ed50a0da84021bb4c6c494926c50445a
florescente <florescente@gmail.com>
2022-07-23 19:18:08 -0300

    ok

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,9 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 require('dotenv').config()
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.query.secret !== process.env.SECRET_TOKEN) {
     return res
       .status(401)
@@ -16,7 +19,7 @@ export default async function handler(req, res) {
       let folder = id.split('/')
       folder = folder.length > 1 ? `/${folder[0]}` : ''
       const final = `/works${folder}`
-      await res.revalidate(final)
+      await res.unstable_revalidate(final)
       return res.json({ revalidated: true })
     }
     if (note === 'delete') {
@@ -24,10 +27,10 @@ export default async function handler(req, res) {
       let folder = id.split('/')
       folder = folder.length > 1 ? `/${folder[0]}` : ''
       const final = `/works${folder}`
-      await res.revalidate(final)
+      await res.unstable_revalidate(final)
       return res.json({ revalidated: true })
     }
-    await res.revalidate('/works')
+    await res.unstable_revalidate('/works')
     return res.status(501).json({ message: 'Not Implemented', note })
   } catch (err) {
     return res.status(500).json({
