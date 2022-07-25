49d9697a0e30080022e56ae0e6cdb070d1534c89
florescente <florescente@gmail.com>
2022-07-24 22:33:10 -0300

    doesnotwxist

---
+++ pages/500.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+function FiveHundread() {
+  return (
+    <>
+      <h1>Internal Error</h1>
+      <Link href="/">Go back home</Link>
+    </>
+  )
+}
+
+export default FiveHundread
