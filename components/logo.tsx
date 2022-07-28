735ce459b99e0fe218944b3b71feae9f02d493c4
florescente <florescente@gmail.com>
2022-07-28 01:33:56 -0300

    center name

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,7 +3,12 @@ import { LogoText } from '../interfaces/trampos'
 
 function Logo({ title, sub }: LogoText) {
   return (
-    <Box display="flex" flexDirection="column" marginX={3}>
+    <Box
+      display="flex"
+      flexDirection="column"
+      marginX={3}
+      alignItems={{ xs: 'center', sm: 'flex-start' }}
+    >
       <Typography variant="h1">{title}</Typography>
       <Typography marginBottom={3}>{sub}</Typography>
     </Box>
